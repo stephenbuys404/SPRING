13865439cb9bc5a6f191d795ad6e06268998c699
unknown <stephenbuys404@gmail.com>
2023-06-29 12:46:37 +0200

    source

---
+++ projects/corona-tracker/src/test/java/io/javabrains/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,11 @@
+package io.javabrains.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
